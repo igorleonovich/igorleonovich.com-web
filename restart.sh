deb39b6df78bc09e69487ea00676fb82ec0a5eb9
Igor Leonovich <8408481+igorleonovich@users.noreply.github.com>
2023-09-23 05:37:46 +0400

    Update Docker container control scripts

--- restart.sh
+++ restart.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+SCRIPT_PATH=$(realpath $0)
+PROJECT_PATH=$(dirname $SCRIPT_PATH)
+
 docker network create nginx-proxy
-docker compose --env-file ".env" restart
+docker compose -f ${PROJECT_PATH}/docker-compose.yml \
+  --env-file ${PROJECT_PATH}/.env \
+  --project-name "igorleonovich.com-web" \
+  restart
